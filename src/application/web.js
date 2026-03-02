146359d3c46432224c93d0358312b8122934591a
Yazidvcc <yazidkhairuloff@gmail.com>
2026-03-02 09:12:49 +0700

    add middleware static file

--- src/application/web.js
+++ src/application/web.js
@@ -3,9 +3,10 @@ import publicRouter from '../route/public-api.js';
 import errorMiddleware from '../middleware/error-middleware.js';
 import userRouter from '../route/api.js';
 import expressFileUpload from 'express-fileupload'
+import path from "path"
 
 const web = express();
-web.use(express.static("storage"))
+web.use("/mechanic", express.static(path.resolve(__dirname, "../../storage/mechanic")))
 web.use(express.json());
 web.use(expressFileUpload())
 web.use(publicRouter);

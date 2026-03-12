28cd66a402b9c50b03e55cdbd91ef68eb3a7f5e4
Yazidvcc <yazidkhairuloff@gmail.com>
2026-03-12 14:29:02 +0700

    move endpoint get photo item and mechanic from secure api to public api

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -1,8 +1,13 @@
 import express from "express"
 import userController from "../controller/user-controller.js"
+import itemController from "../controller/item-controller.js"
+import mechanicController from "../controller/mechanic-controller.js"
 
 const publicRouter = new express.Router()
 
 publicRouter.post('/api/users/login', userController.login)
+
+publicRouter.get("/api/mechanics/:mechanicId/photo", mechanicController.getPhoto)
+publicRouter.get("/api/items/:itemId/photo", itemController.getPhoto)
 
 export default publicRouter
